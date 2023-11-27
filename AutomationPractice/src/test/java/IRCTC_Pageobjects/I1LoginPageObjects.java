973413c81e3fad82a09f7161998b2f2024c73c99
Ramesh <mail1ramesh77@gmail.com>
2023-11-27 19:36:17 +0530

    Ramesh6

--- AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
+++ AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
@@ -1,12 +1,6 @@
 package IRCTC_Pageobjects;
 
 public class I1LoginPageObjects {
-	public void login() {
-	System.out.println("Ramesh1");
+	
 
-
-	System.out.println("Ramesh10");
-	System.out.println("Ramesh11");
-	System.out.println("Ramesh12");
-	}
 }
