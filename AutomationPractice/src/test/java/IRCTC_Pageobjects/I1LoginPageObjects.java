666b3d576ea862c1b2f3a4de3e73ebc4d18d9aa0
Ramesh <mail1ramesh77@gmail.com>
2023-11-28 18:57:26 +0530

    Ramesh9

--- AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
+++ AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
@@ -1,6 +1,48 @@
 package IRCTC_Pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class I1LoginPageObjects {
 	
+@FindBy(linkText="LOGIN")
+	WebElement login;
 
+@FindBy(xpath="//form/div[1]/input[@type='text']")
+	WebElement Username;
+@FindBy(xpath="//form/div[2]/input[@type='password']")
+WebElement Password;
+@FindBy(linkText="SIGN IN")
+WebElement signin;
+@FindBy(linkText="Logout")
+WebElement logout;
+
+public void Clicklogin() {
+	login.click();
 }
+public void Enterusername(String UserNme) {
+	Username.sendKeys(UserNme);
+}
+public void Enterpassword(String Paswrd) {
+	Password.sendKeys(Paswrd);
+}
+public void Clicksignin() {
+	signin.click();	
+}
+public void Clicklogout() {
+	logout.click();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
