1df16b4b60a02d9fdbb9c3e14cd14814e0b57db7
Ramesh <mail1ramesh77@gmail.com>
2023-11-28 19:11:58 +0530

    Ramesh10

--- AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
+++ AutomationPractice/src/test/java/IRCTC_Pageobjects/I1LoginPageObjects.java
@@ -1,48 +1,47 @@
 package IRCTC_Pageobjects;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class I1LoginPageObjects {
-	
-@FindBy(linkText="LOGIN")
+	WebDriver Idriver;
+
+	public I1LoginPageObjects(WebDriver rDriver) {
+		Idriver = rDriver;
+		PageFactory.initElements(rDriver, this);
+	}
+
+	@FindBy(linkText = "LOGIN")
 	WebElement login;
 
-@FindBy(xpath="//form/div[1]/input[@type='text']")
+	@FindBy(xpath = "//form/div[1]/input[@type='text']")
 	WebElement Username;
-@FindBy(xpath="//form/div[2]/input[@type='password']")
-WebElement Password;
-@FindBy(linkText="SIGN IN")
-WebElement signin;
-@FindBy(linkText="Logout")
-WebElement logout;
+	@FindBy(xpath = "//form/div[2]/input[@type='password']")
+	WebElement Password;
+	@FindBy(linkText = "SIGN IN")
+	WebElement signin;
+	@FindBy(linkText = "Logout")
+	WebElement logout;
 
-public void Clicklogin() {
-	login.click();
+	public void Clicklogin() {
+		login.click();
+	}
+
+	public void Enterusername(String UserNme) {
+		Username.sendKeys(UserNme);
+	}
+
+	public void Enterpassword(String Paswrd) {
+		Password.sendKeys(Paswrd);
+	}
+
+	public void Clicksignin() {
+		signin.click();
+	}
+
+	public void Clicklogout() {
+		logout.click();
+	}
 }
-public void Enterusername(String UserNme) {
-	Username.sendKeys(UserNme);
-}
-public void Enterpassword(String Paswrd) {
-	Password.sendKeys(Paswrd);
-}
-public void Clicksignin() {
-	signin.click();	
-}
-public void Clicklogout() {
-	logout.click();
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
