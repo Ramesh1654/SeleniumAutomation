1ec12a3bffc8b2029bce28454fe5caaf6a437584
Ramesh <mail1ramesh77@gmail.com>
2023-12-21 13:57:39 +0530

    Ramesh22

--- AutomationPractice/src/test/java/IRCTC_StepDefinitionFile/I1LoginStepDefinitionFile.java
+++ AutomationPractice/src/test/java/IRCTC_StepDefinitionFile/I1LoginStepDefinitionFile.java
@@ -1,13 +1,12 @@
 package IRCTC_StepDefinitionFile;
 
+import java.awt.Desktop;
 import java.io.File;
-
+import java.io.IOException;
 import java.time.Duration;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.Assert;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,12 +20,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class I1LoginStepDefinitionFile {
-	static WebDriver driver;
+public class I1LoginStepDefinitionFile extends I1LoginUtilities {
+	// static WebDriver driver;
 	public Logger logger;
 	public I1LoginPageObjects loginpage;
-	public ExtentCasesRreports TestReports;
-	public I1LoginUtilities Utility;
+	// public ExtentCasesRreports TestReports;
+	// public I1LoginUtilities Utility;
 	ChromeOptions options = new ChromeOptions();
 
 	@Given("User Launch the Chrome Browser1")
@@ -42,28 +41,29 @@ public class I1LoginStepDefinitionFile {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		logger.info("**************ChromeBrowser was Lauched****************");
 		loginpage = new I1LoginPageObjects(driver);
-		TestReports = new ExtentCasesRreports();
-		// I1LoginUtilities.CaptureScreenshot("image.jpg");
-		// I1LoginUtilities.captureScreenshot();
+		// System.out.println("driver="+driver); // To cross check driver is initialized
+		// or not
+
 	}
 
 	@When("User Opens the URL {string}")
 	public void user_opens_the_url(String URL) {
+		driver.get("https://www.google.co.in/");
+		getExtentReport("URL 1 Launched", "Google1.jpg", "Description 1");
 		driver.get(URL);
+		getExtentReport("URL 2 Launched", "Google2.jpg", "Description 2");
 		logger.info("**************info: IRCTC Application was Opened************");
 		logger.warn("**************warn: IRCTC Application was Opened************");
 		logger.error("************error: IRCTC Application was Opened************");
 		logger.fatal("************fatal: IRCTC Application was Opened************");
 		logger.debug("************fatal: IRCTC Application was Opened************");
-		I1LoginUtilities.CaptureScreenshot11("image.jpg");
-		I1LoginUtilities.captureScreenshot22();
-		ExtentCasesRreports.Extentmethod();
 
 	}
 
 	@Then("User Click on the Login button")
 	public void user_click_on_the_login_button() {
 		loginpage.Clicklogin();
+		getExtentReport("URL 2A Launched", "Google3.jpg", "Description 3");
 		logger.info("***********User Click on the Login Button*************");
 
 	}
@@ -80,6 +80,7 @@ public class I1LoginStepDefinitionFile {
 	@Then("User Click on the Signin button")
 	public void user_click_on_the_signin_button() throws Throwable {
 		Thread.sleep(15000);
+		getExtentReport("URL 2B Launched", "Google4.jpg", "Description 4");
 		logger.info("****************User will Click on the Signin button************");
 		loginpage.Clicksignin();
 		logger.info("****************User Clicked on the Signin button************");
@@ -88,6 +89,7 @@ public class I1LoginStepDefinitionFile {
 
 	@When("Page Title should be {string}")
 	public void page_title_should_be(String Expectedtitle) throws Throwable {
+		getExtentReport("URL 2C Launched", "Google5.jpg", "Description 5");
 		String Expectedtitle1 = "IRCTC Next Generation eTicketing System";
 		String Actualtitle = driver.getTitle();
 		if (Actualtitle.equals(Expectedtitle1)) {
@@ -100,9 +102,11 @@ public class I1LoginStepDefinitionFile {
 	}
 
 	@When("User Click on the Logout button")
-	public void user_click_on_the_logout_button() {
+	public void user_click_on_the_logout_button() throws Throwable {
 		loginpage.Clicklogout();
 		logger.info("***********User Logout successfully**********");
+		getExtentReport("URL 2D Launched", "Google6.jpg", "Description 6");
 		driver.quit();
+		Desktop.getDesktop().browse(new File("report.html").toURI()); // To open Reports file Automatically from java
 	}
 }
--- AutomationPractice/src/test/java/IRCTC_Utilities/I1LoginUtilities.java
+++ AutomationPractice/src/test/java/IRCTC_Utilities/I1LoginUtilities.java
@@ -1,19 +1,57 @@
 package IRCTC_Utilities;
 
+import java.awt.Desktop;
 import java.io.File;
 
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class I1LoginUtilities {
 	public static WebDriver driver;
-	public static String CaptureScreenshot11(String fileName) {
-		TakesScreenshot takesScreenshot1 = (TakesScreenshot) driver;
-		File sourceFile = takesScreenshot1.getScreenshotAs(OutputType.FILE);
+	static ExtentReports extentreports = new ExtentReports(); // Reporter Engine
+	static File file = new File("report.html");
+	static ExtentSparkReporter spartreporter = new ExtentSparkReporter(file);
+
+	static ExtentTest test1 = extentreports.createTest("TestCaseName 1", "TestCase Description"); // (TC_Name, Description)
+
+
+	public static void getExtentReport(String msg, String path, String Desc) {
+
+		// TODO Auto-generated method stub
+		// ExtentReports extentreports = new ExtentReports(); // Reporter Engine
+		// File file = new File("report.html");
+		//ExtentSparkReporter spartreporter = new ExtentSparkReporter(file);
+		extentreports.attachReporter(spartreporter);
+
+		test1.info(msg);
+		test1.addScreenCaptureFromBase64String(captureScreenshot(), Desc);
+		//test1.addScreenCaptureFromPath(captureScreenshot(path), Desc);
+
+//			test1.addScreenCaptureFromBase64String(getBase64Code, "Google Homepage #1");
+//			test1.addScreenCaptureFromPath(path, "Google Homepage #1");
+		extentreports.flush();
+//		Desktop.getDesktop().browse(new File("report.html").toURI()); // To open Reports file Automatically from java
+	}
+
+	
+	// Capturing Screenshot
+
+	// 1. 'for file path' - To capture screenshot and store it in Local machine
+	public static String captureScreenshot(String fileName) {
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
 		File destFile = new File("./Screenshots/" + fileName);
 		try {
 			FileUtils.copyFile(sourceFile, destFile);
@@ -25,7 +63,8 @@ public class I1LoginUtilities {
 	}
 
 	// 2. 'for Base64Code' - To return Base64 image
-	public static String captureScreenshot22() {
+	public static String captureScreenshot() {
+		System.out.println("driver="+driver);
 		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
 		String base64Code = takesScreenshot.getScreenshotAs(OutputType.BASE64);
 		System.out.println("Screenshot - Base64 image Returned");
